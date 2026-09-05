1c46ec32fc04c9c4089c36c554fff38cadb0c806
Pritam Sharma <qurilotechnologiesllc@gmail.com>
2026-09-05 17:25:54 +0530

    fix the module errro

--- src/routes/adminOrderDashboardRoute.js
+++ src/routes/adminOrderDashboardRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { getAllOrders, getOrderById } = require('../controllers/adminOrderDashboard');
-const { authMiddleware } = require('../middlewares/authMiddleware');
+const { authMiddleware } = require('../middlewares/authmiddleware');
 
 router.get('/orders', authMiddleware, getAllOrders);
 router.get('/orders/:id', authMiddleware, getOrderById);
